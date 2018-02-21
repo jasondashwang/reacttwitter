137e22690b78c40a6882c1e5d43ffc65f7b673ec
Jason Wang <jason@wangjason.com>
2018-02-20 20:02:05 -0500

    done

--- app/js/components/Home.jsx
+++ app/js/components/Home.jsx
@@ -1,14 +1,82 @@
 import React from 'react';
+import Tweet from './Tweet.jsx';
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      tweets: [],
+      tweetIds: new Set(),
+      paused: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.tweetHandler = this.tweetHandler.bind(this);
+  }
+
+  tweetHandler (receivedTweets) {
+    const newTweets = receivedTweets.filter(tweet => {
+      return !this.state.tweetIds.has(tweet.id)
+    });
+
+    const updatedTweets = [...this.state.tweets];
+
+    newTweets.forEach(tweet => {
+      this.state.tweetIds.add(tweet.id);
+      if (updatedTweets.length >= 26) updatedTweets.pop();
+      updatedTweets.unshift(tweet);
+    });
+
+    this.setState({
+      tweets: updatedTweets
+    });
+  }
+
+  componentDidMount () {
+    fetch('http://ec2-18-216-120-197.us-east-2.compute.amazonaws.com:3030/feed/start')
+    .then(res => res.json())
+    .then(result => {
+      this.tweetHandler(result);
+
+      setInterval(() => {
+        if (!this.state.paused) {
+          fetch('http://ec2-18-216-120-197.us-east-2.compute.amazonaws.com:3030/feed/start')
+          .then(res => res.json())
+          .then(this.tweetHandler)
+          .catch(err => {
+            console.error(err);
+          });
+        }
+      }, 3000);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  handleClick (evt) {
+    evt.preventDefault();
+
+    this.setState({
+      paused: !this.state.paused
+    })
   }
 
   render() {
       return (
-        <div>
-	   <b> Component Code Goes Here</b>
+        <div id="feed">
+          <h1>Twitter Feed</h1>
+          <button onClick={this.handleClick} id="btn">{ this.state.paused ? 'Resume' : 'Pause'}</button>
+          <div id="tweets">
+            <hr id="head" />
+            {
+              this.state.tweets.map(tweet => {
+                return <Tweet key={tweet.id} tweet={tweet} />
+              })
+            }
+            <hr id="tail" />
+          </div>
         </div>
     );
   }
--- app/js/components/Tweet.jsx
+++ app/js/components/Tweet.jsx
@@ -1,1 +1,37 @@
 // Create a new React component here!
+import React, { Component } from 'react';
+
+export default class Tweet extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      highlighted: false
+    }
+
+    this.changeColor = this.changeColor.bind(this);
+  }
+
+  changeColor (evt) {
+    evt.preventDefault();
+
+    this.setState({
+      highlighted: !this.state.highlighted
+    })
+  }
+
+  render () {
+    const tweet = this.props.tweet;
+
+    return (
+      <div className="tweet" style={{
+        backgroundColor: this.state.highlighted ? 'yellow' : 'white'
+      }}>
+        <img src={tweet.user.profile_image_url} />
+        <h4>${tweet.user.name}</h4>
+        <p>${tweet.text}</p>
+        <button onClick={this.changeColor}>{this.state.highlighted ? 'Unhighlight' : 'Highlight'} Tweet</button>
+      </div>
+    );
+  }
+}
